7684c86f6f5f842b3c0927992768c228782ce7a1
Jerry Lee <lee@idoctor.co.jp>
2019-06-06 13:48:34 +0900

    add file upload area

---
+++ public/script.js
@@ -0,0 +1,28 @@
+($ => {
+  const FILE_SIZE_LIMIT = 2 // 2mega
+  $(() => {
+    $(document)
+      .on('click', '#photoBtn', () => {
+        $('#photoInput').click()
+      })
+      .on('change', '#photoInput', ev => {
+        const el = ev.currentTarget
+        const file = el.files[0]
+        if (file) {
+          const fileSize = file.size
+          if (fileSize > 1024 * 1024 * FILE_SIZE_LIMIT) {
+            alert(`写真データを ${FILE_SIZE_LIMIT}m 以下にしてください！`)
+            el.value = ''
+          } else {
+            let fileName = file.name
+            if (fileName.length > 10) {
+              fileName = '...' + fileName.substr(10)
+            }
+            $('#photoBtn').text(fileName)
+          }
+        } else {
+          $('#photoBtn').text('写真をアップ')
+        }
+      })
+  })
+})(jQuery)
