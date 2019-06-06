4003abe63b1afce3113a37270eacad29a42b8a21
Jerry Lee <lee@idoctor.co.jp>
2019-06-06 12:37:41 +0900

    load file as erb

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 # coding: utf-8
+require 'date'
+require 'erb'
 require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/' do
   @title = "YAML to 履歴書"
+  @date = Date.today
+  @data_yml = load_as_erb("public/data.yaml")
+  @style_txt = load_as_erb("public/style.txt")
   erb :index
 end
+
+private
+
+  def load_as_erb(file_path)
+    ERB.new(File.read(file_path)).result(binding)
+  end
